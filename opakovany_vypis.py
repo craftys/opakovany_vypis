70344566be738870d07138038f8d8fd39ae91ee2
Miroslav Janota <craftys@seznam.cz>
2018-07-22 21:05:43 +0200

    Prvni revize

---
+++ opakovany_vypis.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Vzorový program python
+"""
+
+def vypis(a):
+    for b in range(a):
+        print("číslo -", b)
+        
+a=int(input("Zadej počet opakování: "))
+vypis(a)
+       
